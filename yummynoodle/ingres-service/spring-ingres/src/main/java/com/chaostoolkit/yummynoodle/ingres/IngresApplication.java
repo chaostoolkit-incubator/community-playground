9f98dacc5e2d92a9390159afbed8e582cbbb58bf
Russ Miles <russ@russmiles.com>
2019-04-29 13:11:27 +0100

    Added initial generated ingres service
    Signed-off-by: Russ Miles <russ@russmiles.com>

---
+++ yummynoodle/ingres-service/spring-ingres/src/main/java/com/chaostoolkit/yummynoodle/ingres/IngresApplication.java
@@ -0,0 +1,13 @@
+package com.chaostoolkit.yummynoodle.ingres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngresApplication.class, args);
+	}
+
+}
---
+++ yummynoodle/ingres-service/spring-ingres/src/test/java/com/chaostoolkit/yummynoodle/ingres/IngresApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chaostoolkit.yummynoodle.ingres;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IngresApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
