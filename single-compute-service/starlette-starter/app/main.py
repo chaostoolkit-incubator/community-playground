5cbdb3e7468088b7e2b723a419d3ea4b35e570f9
Grant TF <grant@chaosiq.io>
2019-11-04 17:12:09 +0000

    add Home page for demo App (#38)
    This includes some links and references to ChaosIQ Resources and some Tweet links
    Added option to run from python locally
    Closes #36
    
    Signed-off-by: gtfisher <grant@chaosiq.io>

--- single-compute-service/starlette-starter/app/main.py
+++ single-compute-service/starlette-starter/app/main.py
@@ -2,19 +2,33 @@ import datetime
 import sys
 
 from starlette.applications import Starlette
+from starlette.staticfiles import StaticFiles
 from starlette.responses import JSONResponse
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+import uvicorn
 
-version = "{sys.version_info.major}.{sys.version_info.minor}"
+version = f"{sys.version_info.major}.{sys.version_info.minor}"
+templates = Jinja2Templates(directory='templates')
 
 app = Starlette()
+app.mount('/static', StaticFiles(directory='statics'), name='static')
 
-
-@app.route("/")
+@app.route('/')
 async def homepage(request):
-    message = f"Hello world! From Starlette running on Uvicorn with Gunicorn in Alpine. Using Python {version}"
-    return JSONResponse({"message": message})
+    template = "index.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context)
+
+@app.route("/msg")
+async def message(request):
+    message = f"Hello world! From Starlette running on Uvicorn with Gunicorn in Alpine."
+    return JSONResponse({"message": message, "version": version})
 
 @app.route('/dt')
 async def mydatetime(request):
     print(datetime.datetime.now())
     return JSONResponse({'hello': 'world', 'now': str(datetime.datetime.now())})
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
